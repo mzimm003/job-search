bc7fd6ee794c8ce125703054d6a815b7f88ad979
Mark <mzimmerman38@gatech.edu>
2023-11-22 13:22:48 -0800

    Revamping Search

--- Search/search.py
+++ Search/search.py
@@ -6,36 +6,55 @@ from typing import (
 from functools import partial
 from Search.utility import (
     request,
-    getlinks
+    getlinks,
+    getDesc
 )
 
 class Search:
     def __init__(
             self,
             orgName:str = '',
-            method:List[Callable] = [],
+            listJobsMethod:List[Callable] = [],
+            getJobDescMethod:List[Callable] = [],
             searchReq:Dict = {},
+            searchPhrases:List[str] = '',
             retType:str = 'json'
             ) -> None:
         self.orgName = orgName
-        self.method = method
+        self.listJobsMethod = listJobsMethod
+        self.getJobDescMethod = getJobDescMethod
         self.searchReq = searchReq
+        self.searchPhrases = searchPhrases
         self.retType = retType
     
-    def getJobList(self) -> List[str]:
+    def listJobs(self) -> List[str]:
         '''
         return ...string links?... to job postings
         '''
-        return self.runMethod()
+        jobs = []
+        for srchPhrs in self.searchPhrases:
+            
+        return self.runMethod(self.listJobsMethod, self.searchReq)
+    
+    def getJobDesc(self, link) -> List[str]:
+        reqDict = {
+            'method':'GET',
+            'url':link,
+            'headers':'{"User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0"}'
+            }
+        return self.runMethod(self.listJobsMethod, reqDict)
 
-    def runMethod(self):
+    def runMethod(self, method, reqDict):
         x = None
-        for i, m in enumerate(self.method):
+        for i, m in enumerate(method):
             if i == 0:
-                x = m()
+                x = m(reqDict)
             else:
                 x = m(x)
         return x
+
+    def addSearchPhrase(self, phrase):
+        self.searchPhrases.append(phrase)
 
     @classmethod
     def byJSON(
@@ -53,14 +72,19 @@ class Search:
         cls,
         searchReq:Dict = {},
         jobKeyId = '',
+        descKey = '',
         ):
         obj = cls()
         orgName = searchReq['url'].lstrip('https://')
         obj.orgName = orgName[:orgName.find('/')]
-        obj.method = [
-                partial(request, searchReq=searchReq),
-                partial(getlinks, keyword=jobKeyId),
+        obj.listJobsMethod = [
+            partial(request),
+            partial(getlinks, keyword=jobKeyId),
             ]
-        # obj.searchReq = searchReq
+        obj.searchReq = searchReq
+        obj.getJobDescMethod = [
+            partial(request),
+            partial(getDesc, descKey=descKey)
+            ]
         obj.retType = 'html'
         return obj
--- Search/utility.py
+++ Search/utility.py
@@ -13,8 +13,13 @@ def request(searchReq:Dict):
 def getlinks(
         webpageResp:requests_html.HTMLResponse,
         keyword:str):
-        l = []
-        for link in webpageResp.html.absolute_links:
-            if keyword in link:
-                l.append(link)
-        return l
+    l = []
+    for link in webpageResp.html.absolute_links:
+        if keyword in link:
+            l.append(link)
+    return l
+
+def getDesc(
+        webpageResp:requests_html.HTMLResponse,
+        keyword:str):
+    return webpageResp.html.find(keyword)[0].text
