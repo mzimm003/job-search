e2446b3e393f0e90010b24d3d6402b06e1038da9
Mark <mzimmerman38@gatech.edu>
2023-11-22 11:01:55 -0800

    Arranging hierarchy. Enabled multiwindow functionality.

--- Search/profile.py
+++ Search/profile.py
@@ -1,36 +1,55 @@
 from typing import (
     List,
+    Dict
 )
 from Search.search import Search
 
+class Portfolio:
+    def __init__(self) -> None:
+        self.profiles:Dict[str, Profile] = {}
+        self.currentPosts = {}
+        self.historicalPosts = {}
+    
+    def addProfile(self, prof:'Profile'):
+        self.profiles[prof.name] = prof
+    
+    def getNewJobs(self):
+        for n, p in self.profiles.items():
+            p.gatherPosts()
+            #TODO
 
 class Profile:
     def __init__(
             self,
             name:str,
+            searches:List[Search] = [],
             ) -> None:
         self.name = name
-        self.searches : List[Search] = []
-        self.currentPosts : List = None
+        self.searches : List[Search] = searches
+        self.currentPosts : Dict[str,'Posting'] = {}
+        self.historicalPosts : Dict[str,'Posting'] = {}
+
+    @classmethod
+    def bySearch(cls, search:Search):
+        return cls(name=search.orgName, searches=search)
 
     def addSearch(self, search:Search):
         self.searches.append(search)
-
-    def populateSearches(self):
-        #TODO
-        x = None
-        for i in x:
-            self.incorporateSearch()
     
     def gatherPosts(self):
         self.currentPosts = []
         for s in self.searches:
             jobs = s.getJobList()
             for job in jobs:
-                self.currentPosts.append(job)
+                self.currentPosts[job] = Posting.byLink(job)
+    
+    def getSearches(self):
+        return self.searches
 
     def processPosts(self):
-        pass
+        for cp in self.currentPosts:
+            pass
+
 
 class Posting:
     def __init__(
@@ -38,8 +57,16 @@ class Posting:
             link,
             ) -> None:
         self.link = link
+    
+    @classmethod
+    def byLink(cls, link):
+        pass
+
 
     def getDesc(self):
+        pass
+
+    def process(self):
         pass
 
     def engageChatGPT(self):
--- gui.py
+++ gui.py
@@ -1,106 +1,294 @@
 import PySimpleGUI as sg
+
 from Search.transforms import Transform
-from Search.profile import Profile
+from Search.profile import (
+    Profile,
+    Portfolio
+)
 from Search.search import Search
+from Search.utility import request
+
 import pickle
 from pathlib import Path
 from typing import (
     Dict
 )
+from functools import partial
 
-def errorMsg(message, window):
-    lyt = [
-        [sg.Text(message)],
-        [sg.Button("OK")]
+class GUI:
+    def __init__(self, portfolio:Portfolio) -> None:
+        self.windows:Dict[str, sg.Window] = {}
+        self.primaryWindow:sg.Window = None
+        self.portfolio:Portfolio = portfolio
+
+    def __newWindow(self, name, layout, modal=False, enable_close_attempted_event=False, resizable=False):
+        loc = (None, None)
+        if self.primaryWindow:
+            loc = self.primaryWindow.current_location()
+        w = sg.Window(
+            name,
+            layout,
+            modal=modal,
+            location=loc,
+            enable_close_attempted_event=enable_close_attempted_event,
+            resizable=resizable,
+            finalize=True)
+        if modal:
+            while True:
+                e, v = w.read()
+                if e == "OK" or e == sg.WIN_CLOSED:
+                    break
+            w.close()
+        else:
+            return w
+    
+    def __closeWindow(self, w:sg.Window):
+        del self.windows[w]
+        w.close()
+    
+    def closeAllWindows(self):
+        wins = list(self.windows.keys())
+        for w in wins:
+            self.__closeWindow(w)
+
+    MAINWINDOWNAME = "Work"
+    NEWPROFILE = '--New--'
+    JOBSDETAIL = 'Detail'
+    ALLNEWJOBS = 'Get All New Jobs'
+    OPENPROFILE = "Access Profile"
+    @staticmethod
+    def MAINWINDOWLAYOUT(profiles):    
+        profile_panel_layout = sg.Col([        
+            [sg.Text("Profiles")],
+            [sg.Listbox([k for k in profiles.keys()], expand_x=True, expand_y=True, key="PROFILES")],
+            [sg.Button(GUI.OPENPROFILE)]
+            ], expand_x=True, expand_y=True)
+        jobs_panel_layout = sg.Col([
+            [sg.Text("Jobs")],
+            [sg.vtop(sg.Button(GUI.JOBSDETAIL)), sg.Col([[sg.Button(GUI.ALLNEWJOBS)],
+                    [sg.Multiline(size=(100,50), key="NEWJOBS")]], pad=((0,0),(0,1)))],
+            ], expand_x=True, expand_y=True)
+
+        layout_l = [[profile_panel_layout]]
+        layout_c = [[]]
+        layout_r = [[jobs_panel_layout]]
+
+        return [
+            [
+                sg.Col(layout_l, expand_x=True, expand_y=True),
+                sg.Col(layout_r, expand_x=True, expand_y=True)
+            ]
         ]
-    w = sg.Window("Error", lyt, modal=True, location=window.current_location())
-    while True:
-        e, v = w.read()
-        if e == "OK" or e == sg.WIN_CLOSED:
-            break
-    w.close()
+    
+    def mainWindow(self):
+        w = self.__newWindow(GUI.MAINWINDOWNAME, GUI.MAINWINDOWLAYOUT(self.portfolio.profiles), enable_close_attempted_event=True, resizable=True)
+        self.primaryWindow = w
+        self.windows[w] = {
+            GUI.OPENPROFILE:self.openProfile,
+            sg.WIN_X_EVENT:self.endProgram,
+            GUI.JOBSDETAIL:..., #TODO
+            GUI.ALLNEWJOBS:... #TODO
+            }
+    
+    def openProfile(self, values):
+        for p in values['PROFILES']:
+            self.profileWindow(p)
+    
+    def endProgram(self, values):
+        lyt = [
+            [sg.Text('Save Work?')],
+            [sg.Button("Yes"),sg.Button("No")]
+            ]
+        save_window = sg.Window("Save?", lyt, modal=True, location=self.primaryWindow.current_location())
+        while True:
+            e, v = save_window.read()
+            if e == "Yes":
+                with open("Search/profiles.pkl", "wb") as f:
+                    pickle.dump(self.portfolio, f)
+                break
+            if e == "No" or e == sg.WIN_CLOSED:
+                break
+        save_window.close()
+        return 'CLOSEALL'
+
+    PEEKLINKS = "Peek Links"
+    ADDSEARCH = "Add Search"
+    @staticmethod
+    def PROFILESWINDOWLAYOUT():
+        header_panel_layout = sg.Col([
+            [sg.Text("Header")],
+            [sg.Multiline(key="SEARCH", expand_x=True, expand_y=True)],
+            [sg.Text("Job Identifying Keyword")],
+            [sg.In(key="KEYID", expand_x=True)],
+            ], expand_x=True, expand_y=True)
+        link_peak_layout = sg.Col([
+            [sg.Text("Job Identifying Keyword Help")],
+            [sg.Listbox([], expand_x=True, expand_y=True, key="LINKS")],
+            [sg.Button(GUI.PEEKLINKS)]
+            ], expand_x=True, expand_y=True)
+        search_panel_layout = sg.Col([
+            [sg.Text("Search Parameters")],
+            [header_panel_layout, link_peak_layout],
+            [sg.Button(GUI.ADDSEARCH)]
+        ], expand_x=True, expand_y=True)
+        
+
+        layout_l = [[search_panel_layout]]
+        layout_c = [[]]
+        layout_r = [[]]
+
+        return [
+            [
+                sg.Col(layout_l, expand_x=True, expand_y=True),
+            ]
+        ]
+            
+    def profileWindow(self, profile:str):
+        w = self.__newWindow(profile, GUI.PROFILESWINDOWLAYOUT(), resizable=True)
+        self.windows[w] = {
+            GUI.ADDSEARCH:partial(self.addSearch, window=w, profile=self.portfolio.profiles[profile]),
+            GUI.PEEKLINKS:partial(self.peekLinks, window=w)
+            }
+
+    def peekLinks(self, values, window:sg.Window):
+        if values['SEARCH'] == '':
+            self.errorMsg("Must fill the search parameter 'Header'")
+        else:
+            search_header = Transform().GUIRequestHeaderToRequestParamDict(values['SEARCH'])
+            search = request(searchReq=search_header)
+            window['LINKS'].update([l for l in search.html.links])
+
+    def addSearch(self, values, window:sg.Window, profile:Profile):
+        if values['SEARCH'] == '' or values['KEYID'] == '':
+            self.errorMsg("Must fill the search parameters, 'Header' and 'Job Indetifying Keyword'")
+        else:
+            search_header = Transform().GUIRequestHeaderToRequestParamDict(values['SEARCH'])
+            search = Search.byHTML(searchReq=search_header, jobKeyId=values['KEYID'])
+            if profile.name != GUI.NEWPROFILE:
+                profile.addSearch(search)
+            else:
+                newProf = Profile.bySearch(search)
+                self.portfolio.addProfile(newProf)
+                self.primaryWindow['PROFILES'].update([k for k in self.portfolio.profiles.keys()])
+                self.profileWindow(newProf.name)
+                self.__closeWindow(window)
+
+
+    def errorMsg(self, message):
+        lyt = [
+            [sg.Text(message)],
+            [sg.Button("OK")]
+            ]
+        self.__newWindow("Error", lyt, modal=True)
+    
+    def run(self):
+        # Create an event loop
+        keepOpen = True
+        while keepOpen:
+            window, event, values = sg.read_all_windows()
+            result = None
+            if event == sg.WIN_CLOSED:
+                if window is self.primaryWindow:
+                    result = self.endProgram(values)
+                else:
+                    self.__closeWindow(window)
+            else:
+                result = self.windows[window][event](values)
+
+            # if event == GUI.ADDSEARCH or event == "Create Profile from Search":
+            #     if values['SEARCH'] == '' or values['KEYID'] == '':
+            #         errorMsg("Must fill the search parameters, 'Header' and 'Job Indetifying Keyword'", window)
+            #     else:
+            #         search_header = Transform().GUIRequestHeaderToRequestParamDict(values['SEARCH'])
+            #         search = Search.byHTML(searchReq=search_header, jobKeyId=values['KEYID'])
+            #         if event == GUI.ADDSEARCH:
+            #             for p in values['PROFILES']:
+            #                 if p != NEWPROFILE:
+            #                     port.profiles[p].addSearch(search)
+            #         else:
+            #             port.addProfile(Profile.bySearch(search))
+            #             window['PROFILES'].update([k for k in port.profiles.keys()])
+
+
+            # elif event == GUI.JOBSDETAIL:
+            #     jobs_window = sg.Window("Jobs", JOBSWINDOWLAYOUT(), modal=True, location=window.current_location())
+            #     while True:
+            #         e, v = jobs_window.read()
+            #         # if e == "Yes":
+            #         #     with open("Search/profiles.pkl", "wb") as f:
+            #         #         pickle.dump(profiles, f)
+            #         #     break
+            #         if e == "No" or e == sg.WIN_CLOSED:
+            #             break
+            #     jobs_window.close()
+            # elif event == GUI.ALLNEWJOBS:
+            #     for p in values['PROFILES']:
+            #         profileWindow(window, p)
+
+
+            if result == 'CLOSEALL':
+                keepOpen = False
+                self.closeAllWindows()
+
 
 def profileWindow(window, profileStr:str, profile:Profile):
-    lyt = [
-        []
-    ]
-    w = sg.Window(profileStr, lyt, location=window.current_location())
+    w = sg.Window(profileStr, PROFILESWINDOWLAYOUT(), location=window.current_location())
+
+def JOBSWINDOWLAYOUT():
+    pass
+    # header_panel_layout = sg.Col([        
+    #     [sg.Text("Header")],
+    #     [sg.Multiline(enable_events=True, key="SEARCH", expand_x=True, expand_y=True)],
+    #     [sg.Text("Job Identifying Keyword")],
+    #     [sg.In(enable_events=True, key="KEYID", expand_x=True)],
+    #     ], expand_x=True, expand_y=True)
+    # link_peak_layout = sg.Col([
+    #     [sg.Text("Job Identifying Keyword Help")],
+    #     [sg.Listbox([], expand_x=True, expand_y=True, key="LINKS")],
+    #     [sg.Button("Peak Links")]
+    #     ], expand_x=True, expand_y=True)
+    # search_panel_layout = sg.Col([
+    #     [sg.Text("Search Parameters")],
+    #     [header_panel_layout, link_peak_layout],
+    #     [sg.Button("Add Search"), sg.Button("Create Profile from Search")]
+    # ], expand_x=True, expand_y=True)
+    
+    # profile_panel_layout = sg.Col([
+    #     [sg.Text("Profiles")],
+    #     [sg.Listbox([k for k in profiles.keys()], expand_x=True, expand_y=True, key="PROFILES")],
+    #     [sg.Button("View Profile")]
+    #     ], expand_x=True, expand_y=True)
+    # jobs_panel_layout = sg.Col([
+    #     [sg.Text("Jobs")],
+    #     [sg.vtop(sg.Button("Detail")), sg.Col([[sg.Button("Get All New Jobs")],
+    #              [sg.Multiline(size=(100,50), key="NEWJOBS")]], pad=((0,0),(0,1)))],
+    #     ], expand_x=True, expand_y=True)
+
+    # layout_l = [[search_panel_layout]]
+    # layout_c = [[]]
+    # layout_r = [
+    #     [profile_panel_layout],
+    #     [jobs_panel_layout]]
+
+    # return [
+    #     [
+    #         sg.Col(layout_l, expand_x=True, expand_y=True),
+    #         sg.Col(layout_r, expand_x=True, expand_y=True)
+    #     ]
+    # ]
 
 
 def main():
-    profiles:Dict[str, Profile] = {}
+    port:Portfolio = Portfolio()
+    port.addProfile(Profile(GUI.NEWPROFILE))
     if Path('Search/profiles.pkl').exists():
         with open('Search/profiles.pkl', 'rb') as f:
-            profiles = pickle.load(f)
+            port = pickle.load(f)
+    gui = GUI(portfolio=port)
 
-    search_panel_layout = [
-        [sg.Text("Search Parameters")],
-        [sg.Text("Header")],
-        [sg.Multiline(enable_events=True, key="SEARCH", expand_x=True, expand_y=True)],
-        [sg.Text("Job Identifying Keyword")],
-        [sg.In(size=(20, 1), enable_events=True, key="KEYID", expand_x=True)],
-        [sg.Button("Add Search"), sg.Button("Create Profile from Search")]
-    ]
-    layout_l = search_panel_layout
+    gui.mainWindow()
 
-    layout_r = [
-        [sg.Listbox([k for k in profiles.keys()], no_scrollbar=True, expand_x=True, expand_y=True, key="PROFILES")],
-        [sg.Button("View Profile")]
-        ]
-
-    layout = [
-        [
-            sg.Col(layout_l, expand_x=True, expand_y=True),
-            sg.Col(layout_r, expand_x=True, expand_y=True)
-        ]
-    ]
-
-    # Create the window
-    window = sg.Window("Work", layout, enable_close_attempted_event=True, resizable=True)
-
-    # Create an event loop
-    while True:
-        event, values = window.read()
-        if event == "Add Search" or event == "Create Profile from Search":
-            if values['SEARCH'] == '' or values['KEYID'] == '':
-                errorMsg("Must fill the search parameters, 'Header' and 'Job Indetifying Keyword'", window)
-            else:
-                search_header = Transform().GUIRequestHeaderToRequestParamDict(values['SEARCH'])
-                search = Search.byHTML(searchReq=search_header, jobKeyId=values['KEYID'])
-                if event == "Add Search":
-                    for p in values['PROFILES']:
-                        profiles[p].addSearch(search)
-                else:
-                    name = search_header['url'].lstrip('https://')
-                    name = name[:name.find('/')]
-                    profiles[name] = Profile(name)
-                    profiles[name].addSearch(search)
-                    window['PROFILES'].update([k for k in profiles.keys()])
-            
-        if event == "View Profile":
-            for p in values['PROFILES']:
-                profileWindow(window, p)
-
-        # End program if user closes window or
-        # presses the OK button
-        if event == "OK" or event == sg.WIN_CLOSED or event == sg.WIN_X_EVENT:
-            lyt = [
-                [sg.Text('Save Work?')],
-                [sg.Button("Yes"),sg.Button("No")]
-                ]
-            save_window = sg.Window("Save?", lyt, modal=True, location=window.current_location())
-            while True:
-                e, v = save_window.read()
-                if e == "Yes":
-                    with open("Search/profiles.pkl", "wb") as f:
-                        pickle.dump(profiles, f)
-                    break
-                if e == "No" or e == sg.WIN_CLOSED:
-                    break
-            save_window.close()
-            break
-
-    window.close()
+    gui.run()
 
 if __name__ == "__main__":
     main()
