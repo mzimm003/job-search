0aa37d4367347361cbef997d3757631323c8381f
mzimm003 <mzimm003@gmail.com>
2023-12-20 19:26:37 -0800

    Attempting GUI library switch

--- gui.py
+++ gui.py
@@ -7,6 +7,7 @@ from Search.profile import (
     Portfolio
 )
 from Search.search import Search
+from Resumes.resume import Resume, Subsection, ResumeRevision
 from Resumes.llm import LLM
 
 import pickle
@@ -22,16 +23,16 @@ import enum
 import tkinter
 
 class GUI:
-    def __init__(self, portfolio:Portfolio, llm:LLM) -> None:
+    def __init__(self, portfolio:Portfolio=None, llm:LLM=None) -> None:
         self.windows:Dict[str, sg.Window] = {}
         self.primaryWindow:sg.Window = None
         self.portfolio:Portfolio = portfolio
         self.llm:LLM = llm
+        
 
-    def __newWindow(self, name, layout, modal=False, enable_close_attempted_event=False, resizable=False, modalEvents={}, modalCloseSet=set(['OK'])):
-        loc = (None, None)
+    def __newWindow(self, name, layout, modal=False, enable_close_attempted_event=False, resizable=False, modalEvents={}, modalCloseSet=set(['OK']), loc = (None,None)):
         ret = None
-        if self.primaryWindow:
+        if loc is None and self.primaryWindow:
             loc = self.primaryWindow.current_location()
         w = sg.Window(
             name,
@@ -459,12 +460,12 @@ class GUI:
         VIEWSITE = enum.auto()
         APPLIED = enum.auto()
         IGNORE = enum.auto()
-        GETTIPS = enum.auto()
-        UPDATERESUME = enum.auto()
         FILTER = enum.auto()
         JOBLIST = enum.auto()
         JOBDESC = enum.auto()
         RESUME = enum.auto()
+        CREATERESUME = enum.auto()
+        # GETTIPS = enum.auto()
     class JOBLISTHEADINGS(enum.Enum):
         Job = enum.auto()
         Company = enum.auto()
@@ -482,20 +483,26 @@ class GUI:
             [sg.Button('View Website', key=GUI.JOBELEMENTS.VIEWSITE)],
             [sg.Button('Applied', key=GUI.JOBELEMENTS.APPLIED),sg.Button('Ignore', key=GUI.JOBELEMENTS.IGNORE)],
             ], expand_x=True, expand_y=True)
-        GPT_layout = sg.Col([
-            [sg.Text("LLM Tips")],
-            [sg.Multiline(key=GUI.JOBELEMENTS.RESUME, expand_x=True, expand_y=True)],
-            [sg.Button('Get Tips', key=GUI.JOBELEMENTS.GETTIPS)],
+        resume_access_layout = sg.Col([
+            [sg.Text("Resume Management")],
+            [sg.Combo(['','Test'], key=GUI.JOBELEMENTS.RESUME, expand_x=True)],
+            [sg.Button('Create Resume', key=GUI.JOBELEMENTS.CREATERESUME)],
             ], expand_x=True, expand_y=True)
-        resume_layout = sg.Col([
-            [sg.Text("Resume Changes")],
-            [sg.Multiline(expand_x=True, expand_y=True)],
-            [sg.Button('Update Resume', key=GUI.JOBELEMENTS.UPDATERESUME)],
-            ], expand_x=True, expand_y=True)
+        
+        # GPT_layout = sg.Col([
+        #     [sg.Text("LLM Tips")],
+        #     [sg.Multiline(key=GUI.JOBELEMENTS.RESUME, expand_x=True, expand_y=True)],
+        #     [sg.Button('Get Tips', key=GUI.JOBELEMENTS.GETTIPS)],
+        #     ], expand_x=True, expand_y=True)
+        # resume_layout = sg.Col([
+        #     [sg.Text("Resume Changes")],
+        #     [sg.Multiline(expand_x=True, expand_y=True)],
+        #     [sg.Button('Update Resume', key=GUI.JOBELEMENTS.UPDATERESUME)],
+        #     ], expand_x=True, expand_y=True)
         
         layout_l = [[job_list_panel_layout]]
         layout_c = [[job_desc_layout]]
-        layout_r = [[GPT_layout],[resume_layout]]
+        layout_r = [[resume_access_layout]]
 
         return [
             [
@@ -515,18 +522,20 @@ class GUI:
             GUI.JOBELEMENTS.IGNORE:partial(self.setJobAsIgnore, window=w),
             GUI.JOBELEMENTS.JOBLIST:partial(self.displayJobDesc, window=w),
             GUI.JOBELEMENTS.VIEWSITE:partial(self.viewWebsite, window=w),
-            GUI.JOBELEMENTS.GETTIPS:partial(self.getTips, window=w),
-            GUI.JOBELEMENTS.UPDATERESUME:...,#TODO
+            GUI.JOBELEMENTS.CREATERESUME:partial(self.createResume, window=w),
             }
         return w
     
-    def getTips(self, values, window:sg.Window):
+    def createResume(self, values, window:sg.Window):
+        #TODO
+        #Need to provide job selection and resume basis selection.
+        #job selection -> where resume json, tex, and pdf will be saved
+        #resume basis -> where resume json will be pulled from, providing available experience in opened resume window
         r = None
         with open('./Resumes/main/resume.pkl','rb') as f:
             r = pickle.load(f)
-        window[GUI.JOBELEMENTS.RESUME].update(r.asString())
+        w = self.resumeWindow('test', r)
 
-        pass
 
     def viewWebsite(self, values, window:sg.Window):
         jobs = window[GUI.JOBELEMENTS.JOBLIST].get()
@@ -575,6 +584,160 @@ class GUI:
                 idxTrack += 1
         window[GUI.JOBELEMENTS.JOBLIST].update(values=table, select_rows=newSelection)
 
+    ####RESUME MODULE####
+    class RESUMEELEMENTS(enum.Enum):
+        EXPERIENCE = enum.auto()
+        EXPKEEP = enum.auto()
+        EXPREVISION = enum.auto()
+        SKILL = enum.auto()
+        SKLKEEP = enum.auto()
+        SKLREVISION = enum.auto()
+        AIEXP = enum.auto()
+        AISKILL = enum.auto()
+        RUNAI = enum.auto()
+        SAVEJSON = enum.auto()
+        SAVEPDF = enum.auto()
+        BACK = enum.auto()
+        NEXT = enum.auto()
+    @staticmethod
+    def RESUMEWINDOWLAYOUT(orgResume:Resume, revResume:ResumeRevision, page=1):
+        revResume.resetMappings()
+
+        class KeyCount:
+            def __init__(self) -> None:
+                self.counts = {k:0 for k in GUI.RESUMEELEMENTS}
+            def getKey(self, k):
+                ret = self.counts[k]
+                self.counts[k] += 1
+                return (k, ret)
+        exp_col_sizes = [(70,3),(5,3),(70,3),(70,3)]
+        label_col_sizes = [(70,1),(5,1),(70,1),(70,1)]
+        def addContent(l:List, subsecs:List[Subsection]):
+            for ss in subsecs:
+                if ss.getType() == Subsection.Types.ORGANIZATION:
+                    l.append([sg.Text(ss.getSubject(), size=label_col_sizes[0]),
+                              sg.Text("", size=label_col_sizes[1]),
+                              sg.Text("", size=label_col_sizes[2]),
+                              sg.Text("", size=label_col_sizes[3]),
+                              ])
+                    addContent(l, ss.elements)
+                elif (ss.getType() == Subsection.Types.POSITION or
+                      ss.getType() == Subsection.Types.PROJECT or
+                      ss.getType() == Subsection.Types.SCHOOL):
+                    l.append([sg.Text('\t'+ss.getSubject(), size=label_col_sizes[0]),
+                              sg.Text("", size=label_col_sizes[1]),
+                              sg.Text("", size=label_col_sizes[2]),
+                              sg.Text("", size=label_col_sizes[3]),])
+                    for e in ss.getElements():
+                        rev_key_keep = keys.getKey(GUI.RESUMEELEMENTS.EXPKEEP)
+                        rev_key_rev = keys.getKey(GUI.RESUMEELEMENTS.EXPREVISION)
+                        revResume.addMapping(rev_key_keep, id(e))
+                        revResume.addMapping(rev_key_rev, id(e))
+                        l.append([sg.Multiline(e, key=keys.getKey(GUI.RESUMEELEMENTS.EXPERIENCE), size=exp_col_sizes[0]),
+                                  sg.Checkbox('', default=revResume.getValueByID(id(e), "Keep"), key=rev_key_keep, size=exp_col_sizes[1]),
+                                  sg.Multiline(revResume.getValueByID(id(e)), key=rev_key_rev, size=exp_col_sizes[2]),
+                                  sg.Multiline("", key=keys.getKey(GUI.RESUMEELEMENTS.AIEXP), size=exp_col_sizes[3]),])
+                        
+                elif ss.getType() == Subsection.Types.SKILL:
+                    l.append([sg.Text(ss.getSubject(), size=label_col_sizes[0]),
+                              sg.Text("", size=label_col_sizes[1]),
+                              sg.Text("", size=label_col_sizes[2]),
+                              sg.Text("", size=label_col_sizes[3]),
+                              ])
+                    if isinstance(ss.getElements()[0], str):
+                        l.append([sg.Multiline(','.join(ss.getElements()), key=keys.getKey(GUI.RESUMEELEMENTS.SKILL), size=exp_col_sizes[0]),
+                                    sg.Checkbox('', default=True, key=keys.getKey(GUI.RESUMEELEMENTS.SKLKEEP), size=exp_col_sizes[1]),
+                                    sg.Multiline(','.join(ss.getElements()), key=keys.getKey(GUI.RESUMEELEMENTS.SKLREVISION), size=exp_col_sizes[2]),
+                                    sg.Multiline("", key=keys.getKey(GUI.RESUMEELEMENTS.AISKILL), size=exp_col_sizes[3]),])
+                    else:
+                        for sss in ss.getElements():
+                            l.append([sg.Text('\t'+sss.getSubject(), size=label_col_sizes[0]),
+                                    sg.Text("", size=label_col_sizes[1]),
+                                    sg.Text("", size=label_col_sizes[2]),
+                                    sg.Text("", size=label_col_sizes[3]),
+                                    ])
+                            l.append([sg.Multiline(','.join(sss.getElements()), key=keys.getKey(GUI.RESUMEELEMENTS.SKILL), size=exp_col_sizes[0]),
+                                    sg.Checkbox('', default=True, key=keys.getKey(GUI.RESUMEELEMENTS.SKLKEEP), size=exp_col_sizes[1]),
+                                    sg.Multiline(','.join(sss.getElements()), key=keys.getKey(GUI.RESUMEELEMENTS.SKLREVISION), size=exp_col_sizes[2]),
+                                    sg.Multiline("", key=keys.getKey(GUI.RESUMEELEMENTS.AISKILL), size=exp_col_sizes[3]),])
+
+        keys = KeyCount()
+        org_resume_layout = []
+        sec = orgResume.getSections()[page]
+        addContent(org_resume_layout, sec.getContent())
+        org_resume_layout += [[sg.Button("Back", key=GUI.RESUMEELEMENTS.BACK),sg.Button("Next", key=GUI.RESUMEELEMENTS.NEXT)]]
+        org_resume_layout = sg.Frame(sec.getTitle(), org_resume_layout)
+
+        org_resume_layout = sg.Col([
+            [sg.Text("", size=label_col_sizes[0]),
+             sg.Text("", size=label_col_sizes[1]),
+             sg.Text("", size=label_col_sizes[2]),
+             sg.Button("Get AI Tips",key=GUI.RESUMEELEMENTS.RUNAI, size=label_col_sizes[3])],
+            [sg.Text("Resume", size=label_col_sizes[0], justification='center'),
+             sg.Text("Keep", size=label_col_sizes[1], justification='center'),
+             sg.Text("Revisions", size=label_col_sizes[2], justification='center'),
+             sg.Text("Rating and Keywords", size=label_col_sizes[3], justification='center')],
+             [org_resume_layout],
+            ],
+            expand_x=True,
+            expand_y=True,
+            # scrollable=True,
+            # vertical_scroll_only=True,
+            # justification='center',
+            # element_justification='center',
+            # size=(300, 200)
+            )
+
+        return [
+            [org_resume_layout],
+            [sg.Button('Update Resume', key=GUI.RESUMEELEMENTS.SAVEJSON),
+            sg.Button('Generate PDF', key=GUI.RESUMEELEMENTS.SAVEPDF)]
+        ]
+        
+
+    def resumeWindow(self, job, orgResume, revResume=None, page=0, loc=(None,None)):
+        if revResume is None:
+            revResume = ResumeRevision.fromResume(orgResume)
+        w = self.__newWindow("Resume - {}".format(job),
+                             GUI.RESUMEWINDOWLAYOUT(orgResume, revResume, page=page),
+                             modal=True,
+                             modalEvents={
+                                 GUI.RESUMEELEMENTS.BACK:partial(
+                                     self.pageResume,
+                                     job=job,
+                                     orgResume=orgResume,
+                                     revResume=revResume,
+                                     page=(page-1)%len(orgResume.getSections())),
+                                 GUI.RESUMEELEMENTS.NEXT:partial(
+                                     self.pageResume,
+                                     job=job,
+                                     orgResume=orgResume,
+                                     revResume=revResume,page=(page+1)%len(orgResume.getSections())),
+                                 },
+                                 modalCloseSet=set([GUI.RESUMEELEMENTS.BACK,
+                                                    GUI.RESUMEELEMENTS.NEXT]),
+                             resizable=True,
+                             loc=loc)
+        return w
+    
+    def pageResume(self, window:sg.Window, event, values, job, orgResume, revResume:ResumeRevision, page):
+        for k, v in values.items():
+            if k[0] == GUI.RESUMEELEMENTS.EXPKEEP:
+                revResume.markKeep(key=k, mark=v)
+            if k[0] == GUI.RESUMEELEMENTS.EXPREVISION:
+                revResume.setRevision(key=k, revision=v)
+        loc = window.CurrentLocation()
+        window.close()
+        self.resumeWindow(job=job, orgResume=orgResume, revResume=revResume, page=page, loc=loc)
+    
+    def getTips(self, values, window:sg.Window):
+        #TODO
+        r = None
+        with open('./Resumes/main/resume.pkl','rb') as f:
+            r = pickle.load(f)
+        tips = self.llm.getTips(r.asString(bulletsOnly=True, skipEdu=True, lineNums=True), values[GUI.JOBELEMENTS.JOBDESC])
+        window[GUI.RESUMEELEMENTS.RESUME].update(tips)
+
     def run(self):
         # Create an event loop
         keepOpen = True
@@ -592,22 +755,6 @@ class GUI:
                         result = self.windows[window][event](values)
             except Exception as inst:
                 self.errorMsg('{}:{}\n{}'.format(type(inst), inst, traceback.format_exc()))
-
-
-            # elif event == GUI.JOBSDETAIL:
-            #     jobs_window = sg.Window("Jobs", JOBSWINDOWLAYOUT(), modal=True, location=window.current_location())
-            #     while True:
-            #         e, v = jobs_window.read()
-            #         # if e == "Yes":
-            #         #     with open("Search/profiles.pkl", "wb") as f:
-            #         #         pickle.dump(profiles, f)
-            #         #     break
-            #         if e == "No" or e == sg.WIN_CLOSED:
-            #             break
-            #     jobs_window.close()
-            # elif event == GUI.ALLNEWJOBS:
-            #     for p in values['PROFILES']:
-            #         profileWindow(window, p)
 
 
             if result == 'CLOSEALL':
